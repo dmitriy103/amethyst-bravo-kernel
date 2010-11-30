c5f9747d22f378f64439780954677a18e3ed36e5
Wu Fengguang <fengguang.wu@intel.com>
2010-11-29 20:49:18 -0600

    nfs: livelock prevention is now done in VFS
    This reverts commit 72cb77f4a5 ("NFS: Throttle page dirtying while we're
    flushing to disk"). The two problems it tries to address
    
    - sync live lock
    - out of order writes
    
    are now all addressed in the VFS
    
    - PAGECACHE_TAG_TOWRITE prevents sync live lock
    - IO-less balance_dirty_pages() avoids concurrent writes
    
    CC: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -337,25 +337,14 @@ static int nfs_writepages_callback(struct page *page, struct writeback_control *
 int nfs_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	struct inode *inode = mapping->host;
-	unsigned long *bitlock = &NFS_I(inode)->flags;
 	struct nfs_pageio_descriptor pgio;
 	int err;
-
-	/* Stop dirtying of new pages while we sync */
-	err = wait_on_bit_lock(bitlock, NFS_INO_FLUSHING,
-			nfs_wait_bit_killable, TASK_KILLABLE);
-	if (err)
-		goto out_err;
 
 	nfs_inc_stats(inode, NFSIOS_VFSWRITEPAGES);
 
 	nfs_pageio_init_write(&pgio, inode, wb_priority(wbc));
 	err = write_cache_pages(mapping, wbc, nfs_writepages_callback, &pgio);
 	nfs_pageio_complete(&pgio);
-
-	clear_bit_unlock(NFS_INO_FLUSHING, bitlock);
-	smp_mb__after_clear_bit();
-	wake_up_bit(bitlock, NFS_INO_FLUSHING);
 
 	if (err < 0)
 		goto out_err;
