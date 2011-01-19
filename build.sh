4781980965e3cd8a6c957ef78430262fbfd0d5f1
stevec99 <stevec@couttsnet.com>
2011-01-19 10:58:24 +0000

    Add BFS builds to build script

--- build.sh
+++ build.sh
@@ -18,6 +18,23 @@ ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_Ginger_CFS_BFQ_SLQB.zip
 
+cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+ARCH=arm make defconfig HAVS_BFS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@925mV_Ginger_BFS_BFQ_SLQB.zip
+
+cp arch/arm/mach-msm/avs.h.875 arch/arm/mach-msm/avs.h
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@875mV_Ginger_BFS_BFQ_SLQB.zip
+
+cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+ARCH=arm make defconfig SVS_BFS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_Ginger_BFS_BFQ_SLQB.zip
+
 git checkout couttstech-2.6.37-froyo
 
 cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
@@ -36,5 +53,22 @@ ARCH=arm make defconfig SVS_defconfig
 ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_Froyo_CFS_BFQ_SLQB.zip
+
+cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+ARCH=arm make defconfig HAVS_BFS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@925mV_Froyo_BFS_BFQ_SLQB.zip
+
+cp arch/arm/mach-msm/avs.h.875 arch/arm/mach-msm/avs.h
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@875mV_Froyo_BFS_BFQ_SLQB.zip
+
+cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+ARCH=arm make defconfig SVS_BFS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_Froyo_BFS_BFQ_SLQB.zip
 
 git checkout couttstech-2.6.37
