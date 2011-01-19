da1315016514fdce7de81a6b2e84e5d1f2ffd47b
stevec99 <stevec@couttsnet.com>
2011-01-19 11:04:03 +0000

    Fix build script

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@875mV_Ginger_BFS_BFQ_SLQB.zip
 
-cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+cp arch/arm/mach-msm/avs.h.875 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig SVS_BFS_defconfig
 ARCH=arm make -j2
 ZipBuilder/builder.sh
@@ -65,10 +65,9 @@ ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_HAVS@875mV_Froyo_BFS_BFQ_SLQB.zip
 
-cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
+cp arch/arm/mach-msm/avs.h.875 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig SVS_BFS_defconfig
 ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1113_Froyo_BFS_BFQ_SLQB.zip
 
-git checkout couttstech-2.6.37
