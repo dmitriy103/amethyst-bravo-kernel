8cfe95e8212f7f42f78520434c163505a9b4225e
stevec99 <stevec@couttsnet.com>
2011-01-28 09:33:22 +0000

    Misc Changes

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Very simple build script
 
+cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig HAVS_defconfig
 ARCH=arm make -j2
@@ -18,8 +19,8 @@ cp arch/arm/mach-msm/avs.h.850 arch/arm/mach-msm/avs.h
 ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS@850mV_Ginger_CFS_VR_SLQB.zip
-cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 
+cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig SVS_defconfig
 ARCH=arm make -j2
@@ -45,6 +46,7 @@ cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_Ginger_BFS_VR
 
 git checkout couttstech-2.6.37-froyo
 
+cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig HAVS_defconfig
 ARCH=arm make -j2
@@ -62,8 +64,8 @@ cp arch/arm/mach-msm/avs.h.850 arch/arm/mach-msm/avs.h
 ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS@850mV_Froyo_CFS_VR_SLQB.zip
-cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 
+cp arch/arm/mach-msm/avs.c.standard arch/arm/mach-msm/avs.c
 cp arch/arm/mach-msm/avs.h.925 arch/arm/mach-msm/avs.h
 ARCH=arm make defconfig SVS_defconfig
 ARCH=arm make -j2
