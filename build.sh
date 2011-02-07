12cfa0203ca1b6237b60fb2ed1e483ae45b168d0
stevec99 <stevec@couttsnet.com>
2011-02-07 13:24:36 +0000

    Add froyo builds to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Very simple build script
 
+git checkout couttstech-2.6.38-ginger
+
 ARCH=arm make defconfig HAVS_defconfig
 ARCH=arm make -j2
 ZipBuilder/builder.sh
@@ -10,4 +12,18 @@ ARCH=arm make defconfig SVS_defconfig
 ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_SVS_Ginger_CFS_VR_SLQB.zip
+
+git checkout couttstech-2.6.38-froyo
+
+ARCH=arm make defconfig HAVS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_HAVS_Froyo_CFS_VR_SLQB.zip
+
+ARCH=arm make defconfig SVS_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_SVS_Froyo_CFS_VR_SLQB.zip
+
+git checkout couttstech-2.6.38-ginger
 
