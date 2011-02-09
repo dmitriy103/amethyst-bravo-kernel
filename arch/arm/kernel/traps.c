fd6940f18a60057a4c9efe3893d52ac900533840
stevec99 <stevec@couttsnet.com>
2011-02-09 10:41:23 +0000

    FIX: warning: "CONFIG_CPU_USE_DOMAINS" is not defined in arch/arm/kernel/traps.c

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -532,7 +532,7 @@ asmlinkage int arm_syscall(int no, struct pt_regs *regs)
 			 * The user helper at 0xffff0fe0 must be used instead.
 			 * (see entry-armv.S for details)
 			 */
-#if CONFIG_CPU_USE_DOMAINS
+#if defined(CONFIG_CPU_USE_DOMAINS)
 			*((unsigned int *)0xffff0ff0) = regs->ARM_r0;
 #else
 			*((unsigned int *)(vectors_page + 0x0ff0)) = regs->ARM_r0;
