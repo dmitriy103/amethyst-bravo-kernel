5f1e62f33ebfae7e2edd9f8574b6f1aaf0d54e96
stevec99 <stevec@couttsnet.com>
2011-02-17 09:06:32 +0000

    Revert "FIX: kernel/sched.c:9348: error: redefinition of 'synchronize_sched_expedited'"
    This reverts commit 3ff07fff988e7e8415cd87ecaf02875fe7dfd3c0.

--- kernel/sched.c
+++ kernel/sched.c
@@ -9343,18 +9343,15 @@ struct cgroup_subsys cpuacct_subsys = {
 };
 #endif	/* CONFIG_CGROUP_CPUACCT */
 
-#ifndef CONFIG_SMP 
+#ifndef CONFIG_SMP
 
-#ifndef CONFIG_TINY_RCU
 void synchronize_sched_expedited(void)
 {
         barrier();
 }
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
-#endif
 
 #else /* #ifndef CONFIG_SMP */
-#ifndef CONFIG_TINY_RCU
 
 static atomic_t synchronize_sched_expedited_count = ATOMIC_INIT(0);
 
@@ -9414,6 +9411,5 @@ void synchronize_sched_expedited(void)
 }
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
 
-#endif
 #endif /* #else #ifndef CONFIG_SMP */
 
