fc6d462b0badf5473b19d287e4dcf013d7f5574d
Steven Coutts <stevec@couttsnet.com>
2011-03-11 13:07:54 +0000

    Misc updates to build stuff

--- build_ame.sh
+++ build_ame.sh
@@ -3,19 +3,19 @@
 
 ARCH=arm make clean
 ARCH=arm make defconfig HAVS_AME_defconfig
-ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS_Amethyst_CFS_BFQ2_SLQB.zip
 
 ARCH=arm make defconfig SVS_AME_defconfig
-ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_SVS_Amethyst_CFS_BFQ2_SLQB.zip
 
 ARCH=arm make defconfig HAVS_BFS_AME_defconfig
 cp arch/arm/Kconfig arch/arm/Kconfig.old
 cp arch/arm/Kconfig.bfs arch/arm/Kconfig
-ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS_Amethyst_BFS_BFQ2_SLQB.zip
 mv arch/arm/Kconfig.old arch/arm/Kconfig
@@ -23,7 +23,7 @@ mv arch/arm/Kconfig.old arch/arm/Kconfig
 ARCH=arm make defconfig SVS_BFS_AME_defconfig
 cp arch/arm/Kconfig arch/arm/Kconfig.old  
 cp arch/arm/Kconfig.bfs arch/arm/Kconfig
-ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_SVS_Amethyst_BFS_BFQ2_SLQB.zip
 mv arch/arm/Kconfig.old arch/arm/Kconfig
