f1602ccf3040f06cab2db935d6da12f946cc2794
Steven Coutts <stevec@couttsnet.com>
2011-03-11 12:57:28 +0000

    Add build stuff for indivdual kernel builds

---
+++ build_ame.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Very simple build script
+
+ARCH=arm make clean
+ARCH=arm make defconfig HAVS_AME_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS_Amethyst_CFS_BFQ2_SLQB.zip
+
+ARCH=arm make defconfig SVS_AME_defconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_SVS_Amethyst_CFS_BFQ2_SLQB.zip
+
+ARCH=arm make defconfig HAVS_BFS_AME_defconfig
+cp arch/arm/Kconfig arch/arm/Kconfig.old
+cp arch/arm/Kconfig.bfs arch/arm/Kconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_HAVS_Amethyst_BFS_BFQ2_SLQB.zip
+mv arch/arm/Kconfig.old arch/arm/Kconfig
+
+ARCH=arm make defconfig SVS_BFS_AME_defconfig
+cp arch/arm/Kconfig arch/arm/Kconfig.old  
+cp arch/arm/Kconfig.bfs arch/arm/Kconfig
+ARCH=arm make -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.37_OC1190_SVS_Amethyst_BFS_BFQ2_SLQB.zip
+mv arch/arm/Kconfig.old arch/arm/Kconfig
+
+ARCH=arm make clean
