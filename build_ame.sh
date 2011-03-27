2aa592d82affa8d4e823ceccf133014436517977
Steven Coutts <stevec@couttstech.com>
2011-03-27 12:59:34 +0100

    Update build script

--- build_ame.sh
+++ build_ame.sh
@@ -5,19 +5,19 @@ rm -r ZipBuilder/system/lib/modules/*
 
 ARCH=arm make clean
 ARCH=arm make defconfig HAVS_AME_defconfig
-CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/phone/kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_HAVS_Amethyst_CFS_BFQ2_SLQB.zip
 
 ARCH=arm make defconfig SVS_AME_defconfig
-CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/phone/kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_SVS_Amethyst_CFS_BFQ2_SLQB.zip
 
 ARCH=arm make defconfig HAVS_BFS_AME_defconfig
 cp arch/arm/Kconfig arch/arm/Kconfig.old
 cp arch/arm/Kconfig.bfs arch/arm/Kconfig
-CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/phone/kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_HAVS_Amethyst_BFS_BFQ2_SLQB.zip
 mv arch/arm/Kconfig.old arch/arm/Kconfig
@@ -25,7 +25,7 @@ mv arch/arm/Kconfig.old arch/arm/Kconfig
 ARCH=arm make defconfig SVS_BFS_AME_defconfig
 cp arch/arm/Kconfig arch/arm/Kconfig.old  
 cp arch/arm/Kconfig.bfs arch/arm/Kconfig
-CROSS_COMPILE=/home/stevec/dev/android-ndk-r5/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin/arm-eabi- ARCH=arm make -j2
+CROSS_COMPILE=/home/stevec/dev/phone/kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm make -j2
 ZipBuilder/builder.sh
 cp ZipBuilder/update_signed.zip ~/Dropbox/couttstech-2.6.38_OC1190_SVS_Amethyst_BFS_BFQ2_SLQB.zip
 mv arch/arm/Kconfig.old arch/arm/Kconfig
